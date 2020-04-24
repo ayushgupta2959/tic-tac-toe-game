6cddf18104a169439ecc52ad8da211b818bedb33
ayushgupta2959 <37008291+ayushgupta2959@users.noreply.github.com>
2020-04-24 21:44:35 +0530

    created basic layout and added styling

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
 import React from 'react';
 import './App.css';
 
-const Cell = () => {
-  return <td></td>;
+const Cell = (props) => {
+  return <td className={props.display}></td>;
 };
 
 const Board = () => {
   return (
     <table>
       <tr>
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell key={1} display={''} />
+        <Cell key={2} display={'vertical'} />
+        <Cell key={3} display={''} />
       </tr>
       <tr>
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell key={4} display={'horizontal'} />
+        <Cell key={5} display={'horizontal vertical'} />
+        <Cell key={6} display={'horizontal'} />
       </tr>
       <tr>
-        <Cell />
-        <Cell />
-        <Cell />
+        <Cell key={7} display={''} />
+        <Cell key={8} display={'vertical'} />
+        <Cell key={9} display={''} />
       </tr>
     </table>
   );
 };
 
-const App = () => {
+const Status = (props) => {
   return (
     <>
-      <h1>Tic Tak Toe</h1>
-      {/* <table>
-        <tr>
-          <td></td>
-          <td class="vertical"></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td class="horizontal"></td>
-          <td class="vertical horizontal"></td>
-          <td class="horizontal"></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td class="vertical"></td>
-          <td></td>
-        </tr>
-      </table> */}
-      <Board />
+      <div className={'status '+props.status}>{props.status}</div>
     </>
   );
+};
+
+const Reset = () => {
+  return (<button className={'reset'}>Reset</button>)
+}
+
+const Game = () => {
+  return (
+    <div className="container">
+      <h1>Tic Tac Toe</h1>
+      <Board />
+      <Status status={'match tie'} />
+      <Reset />
+    </div>
+  );
+};
+
+const App = () => {
+  return <Game />;
 };
 
 export default App;
